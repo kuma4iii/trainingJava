553c6ca09d2f1a75e804996f58b3de6f0b963e6b
Ryota Kumashiro <ryota.kumashiro@gmail.com>
2022-09-16 17:01:38 +0900

    課題１６の修正例を作成

--- javaKadai/RockPaperScissors.java
+++ javaKadai/RockPaperScissors.java
@@ -3,74 +3,66 @@ package javaKadai;
 import java.util.Scanner;
 import java.util.Random;
 
-class RockPaperScissors {
-     /*総合１６
-     *じゃんけんプログラムを作成してください。
-     * 自分の手（　0（グー）、1（パー）、2（チョキ）のいずれかの値）をキーボードから
+class RockPaperScissorsSample {
+    /*
+     * 総合１６
+     * じゃんけんプログラムを作成してください。
+     * 自分の手（ 0（グー）、1（パー）、2（チョキ）のいずれかの値）をキーボードから
      * 入力し、相手の手をランダムで生成し勝負します。
      * あいこの場合には勝負がつくまで繰り返します。
      * 
      * プログラム名：RockPaperScissors.java
      */
+    public static void main(String args[]) {
+        System.out.println("「じゃんけん…」");
 
-     private static void loseMessage() {
-         System.out.println("あなたの負け");
-     }
-     
-     private static void winMessage() {
-         System.out.println("あなたの勝ち");
-     }
-     
-     private static String handsAction(int hand) {
-        String str;
-             if (hand == 0) {
-                 str = "グー";
-             } else if (hand == 1) {
-                 str = "パー";
-             } else {
-                 str = "チョキ";
-             }
-             return str;
-     }
-     public static void main(String args[]) {
-         System.out.println("「じゃんけん…」");
-        
-         for (int i = 1; i > 0; i++) {
+        for (int i = 1; i > 0; i++) {
 
-             Scanner sc = new Scanner(System.in);
-             System.out.print("(0（グー）、1（パー）、2（チョキ）のいずれかを入力してください：");
-             int hand = sc.nextInt();
+            System.out.print("(0（グー）、1（パー）、2（チョキ）のいずれかを入力してください：");
+            Scanner sc = new Scanner(System.in);
+            int hand = sc.nextInt();
 
-             if (i >= 2) {
-                 System.out.println("「しょっ！」");
-             } else {
-                 System.out.println("「ぽんっ！」");
-             }
+            if (i >= 2) {
+                System.out.println("「しょっ！」");
+            } else {
+                System.out.println("「ぽんっ！」");
+            }
 
-             Random rand = new Random();
-             int num = rand.nextInt(3);
+            // 相手の手
+            Random rand = new Random();
+            int num = rand.nextInt(3);
+            String otherStr = getHandStr(num);
 
-             String myStr = handsAction(hand);
-             String otherStr = handsAction(num);
-             System.out.println("（あなた：" + myStr + "）");
-             System.out.println("（相手：" + otherStr + "）");
+            // 自分の手
+            String myStr = getHandStr(hand);
 
-             if (hand == num) {
-                 //あいこ
-                 System.out.println("「あいこで…」");
-             } else if ((hand == 0 && num == 1) || (hand == 1 && num == 2) || (hand == 2 && num == 0)) {
-                 //自分グー、相手パーor自分パー、相手チョキor自分チョキ、相手グー
-                 loseMessage();
-                 break;
-             } else if ((hand == 1 && num == 0) || (hand == 2 && num == 1) || (hand == 0 && num == 2)) {
-                 //自分パー、相手グーor自分チョキ、相手パーor自分グー、相手チョキ
-                 winMessage();
-                 break;
-             }
-
-         }
-         
-         
+            // 判定処理
+            System.out.println("（あなた：" + myStr + "）");
+            System.out.println("（相手：" + otherStr + "）");
+            if (hand == num) {
+                // あいこ
+                System.out.println("「あいこで…」");
+            } else {
+                if ((hand == 0 && num == 1) || (hand == 1 && num == 2) || (hand == 2 && num == 0)) {
+                    // 負け
+                    System.out.println("あなたの負け");
+                } else if ((hand == 1 && num == 0) || (hand == 2 && num == 1) || (hand == 0 && num == 2)) {
+                    // 勝ち
+                    System.out.println("あなたの勝ち");
+                }
+                break;
+            }
+        }
     }
-    
+
+    private static String getHandStr(int handNum) {
+        if (handNum == 0) {
+            return "グー";
+        } else if (handNum == 1) {
+            return "パー";
+        } else {
+            return "チョキ";
+        }
+    }
+
 }
