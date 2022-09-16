1e36adafaa2fa49f2a2d0d4d8d4da6c54a11d3b1
kosugi ayane <you@example.com>
2022-09-16 16:53:17 +0900

    総合１６（修正完了）

--- javaKadai/RockPaperScissors.java
+++ javaKadai/RockPaperScissors.java
@@ -13,16 +13,24 @@ class RockPaperScissors {
      * プログラム名：RockPaperScissors.java
      */
 
-     private static void loseMessage(String myStr, String otherStr) {
-         System.out.println("（あなた：" + myStr + "）");
-         System.out.println("（相手：" + otherStr + "）");
+     private static void loseMessage() {
          System.out.println("あなたの負け");
      }
      
-     private static void winMessage(String myStr,String otherStr) {
-        System.out.println("（あなた：" + myStr + "）");
-        System.out.println("（相手：" + otherStr + "）");
-        System.out.println("あなたの勝ち");
+     private static void winMessage() {
+         System.out.println("あなたの勝ち");
+     }
+     
+     private static String handsAction(int hand) {
+        String str;
+             if (hand == 0) {
+                 str = "グー";
+             } else if (hand == 1) {
+                 str = "パー";
+             } else {
+                 str = "チョキ";
+             }
+             return str;
      }
      public static void main(String args[]) {
          System.out.println("「じゃんけん…」");
@@ -39,60 +47,30 @@ class RockPaperScissors {
                  System.out.println("「ぽんっ！」");
              }
 
-             String myStr;
-             if (hand == 0) {
-                 myStr = "グー";
-             } else if (hand == 1) {
-                 myStr = "パー";
-             } else {
-                 myStr = "チョキ";
-             }
-
              Random rand = new Random();
              int num = rand.nextInt(3);
 
-             String otherStr;
-             if (num == 0) {
-                 otherStr = "グー";
-             } else if (num == 1) {
-                 otherStr = "パー";
-             } else {
-                 otherStr = "チョキ";
-             }
+             String myStr = handsAction(hand);
+             String otherStr = handsAction(num);
+             System.out.println("（あなた：" + myStr + "）");
+             System.out.println("（相手：" + otherStr + "）");
 
              if (hand == num) {
                  //あいこ
-                 System.out.println("（あなた：" + myStr + "）");
-                 System.out.println("（相手：" + otherStr + "）");
-             } else if (hand == 0 && num == 1) {
-                 //自分グー、相手パー
-                 loseMessage(myStr, otherStr);
+                 System.out.println("「あいこで…」");
+             } else if ((hand == 0 && num == 1) || (hand == 1 && num == 2) || (hand == 2 && num == 0)) {
+                 //自分グー、相手パーor自分パー、相手チョキor自分チョキ、相手グー
+                 loseMessage();
                  break;
-             } else if (hand == 1 && num == 2) {
-                 //自分パー、相手チョキ
-                 loseMessage(myStr, otherStr);
-                 break;
-             } else if (hand == 2 && num == 0) {
-                 //自分チョキ、相手グー
-                 loseMessage(myStr, otherStr);
-                 break;
-             } else if (hand == 1 && num == 0) {
-                 //自分パー、相手グー
-                 winMessage(myStr, otherStr);
-                 break;
-             } else if (hand == 2 && num == 1) {
-                 //自分チョキ、相手パー
-                 winMessage(myStr, otherStr);
-                 break;
-             } else {
-                 //自分グー、相手チョキ
-                 winMessage(myStr, otherStr);
+             } else if ((hand == 1 && num == 0) || (hand == 2 && num == 1) || (hand == 0 && num == 2)) {
+                 //自分パー、相手グーor自分チョキ、相手パーor自分グー、相手チョキ
+                 winMessage();
                  break;
              }
 
-             System.out.println("「あいこで…」");
-             
          }
+         
+         
     }
     
 }
